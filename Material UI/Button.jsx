6f93b9055efd733a2cb4ac87e5dd88c918578bcf
FaithNjah <faithnjah5@gmail.com>
2022-10-21 20:26:10 +0100

    first commit

---
+++ Material UI/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+const Buttonone = () => {
+  return (
+    <div>
+        <Button variant = 'contained' >Hello world</Button>
+    </div>
+  )
+}
+
+export default Buttonone
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Buttonone from '../Material UI/Button'
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Buttonone />
+    </div>
+  )
+}
+
+export default App
